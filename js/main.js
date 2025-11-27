3c0926c7d414008c8361b65dce0a5b4c2660a65e
István <nagyistvn12@gmail.com>
2025-11-27 23:48:21 +0100

    2025.11.27-6

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-console.log('running...');
-document.getElementById("year").textContent = new Date().getFullYear();
+window.onload = function() {
+  console.log('running...');
 
-function updateStatus() {
-  const dot = document.getElementById('status-dot');
-  const now = new Date();
+  // év beállítása
+  document.getElementById("year").textContent = new Date().getFullYear();
 
-  // CET = UTC+1 (vagy nyáron UTC+2)
-  const utcHour = now.getUTCHours();
-  const isDST = now.getTimezoneOffset() === -120; // nyári időszámítás
-  const cetHour = (utcHour + (isDST ? 2 : 1)) % 24;
+  function updateStatus() {
+    const dot = document.getElementById('status-dot');
+    const now = new Date();
 
-  if (cetHour >= 8 && cetHour <18) {
-    dot.classList.add('online');
-    dot.classList.remove('offline');
-  } else {
-    dot.classList.add('offline');
-    dot.classList.remove('online');
+    const utcHour = now.getUTCHours();
+    const isDST = now.getTimezoneOffset() === -120;
+    const cetHour = (utcHour + (isDST ? 2 : 1)) % 24;
+
+    if (cetHour >= 8 && cetHour < 18) {
+      dot.classList.add('online');
+      dot.classList.remove('offline');
+    } else {
+      dot.classList.add('offline');
+      dot.classList.remove('online');
+    }
   }
-}
 
-// futtatás betöltéskor és percenként
-updateStatus();
-setInterval(updateStatus, 60000);
+  updateStatus();
+  setInterval(updateStatus, 60000);
+};
